d933189bcba765caeff800f91578a9f3f3c3ec31
Leidiane/117 <leidiane5499@hotmail.com>
2023-06-27 23:43:33 -0300

    criando primeiro teste case-cadastro

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,14 +1,20 @@
+
 //testando git//
-describe('Validar acesso a pagina',() => {
-    beforeEach(()=> {
+describe('Validar Página', () => {
+    beforeEach(() => {
+    cy.visit('https://thinking-tester-contact-list.herokuapp.com/')
+}) 
 
-    cy.visit('http://advantageonlineshopping.com')
-    })
+    it('Preencher novo cadastro"', () =>  {
+    cy.get('#signup').click()
+    cy.get('#firstName').type('Leidiane')
+    cy.get('#lastName').type('Souza')
+    cy.get('#email').type('leidiane5499@hotmail.com')
+    cy.get('#password').type('1234567')
+    cy.get('#submit').click()
+})
 
-    it('Cadastrar usuário',() => {
-    cy.get('#menuUserSVGPath').click()
-
-
-    })
 
 })
+
+    
